1051b10733532ea871e572afa08ec4a8f2b6c3ba
006652 <wangning@htsc.com>
2021-07-14 00:16:13 +0800

    util function

--- usbSerialExamples/src/main/java/com/hoho/android/usbserial/util/Utils.java
+++ usbSerialExamples/src/main/java/com/hoho/android/usbserial/util/Utils.java
@@ -1,5 +1,6 @@
 package com.hoho.android.usbserial.util;
 
+import android.content.Context;
 import android.os.Environment;
 
 import java.io.File;
@@ -10,6 +11,27 @@ import java.io.File;
 
 public class Utils {
 
+    /**
+     * 将dp转换成px
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dp2px(Context context, float dpValue){
+        final float scale = context.getResources ().getDisplayMetrics ().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 将像素转换成dp
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dp(Context context,float pxValue){
+        final float scale = context.getResources ().getDisplayMetrics ().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 
     /**
      在Java中:
